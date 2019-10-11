7509706dcb73f12d1290aa133bc59ea0fb395ff5
Raymond <raymondandrew2000@gmail.com>
2019-10-10 19:51:57 -0500

    File was already made for us

--- MesoStation.java
+++ MesoStation.java
@@ -1,2 +1,11 @@
 public class MesoStation {
+    private String stID;
+
+    public MesoStation(String stId) {
+        this.stID = stId;
+    }
+
+    public String getStID() {
+        return stID;
+    }
 }
