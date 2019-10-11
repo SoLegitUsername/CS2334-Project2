75a18dd2f38b35a9d8d4b5a9c1c4667bbb47913b
Raymond <raymondandrew2000@gmail.com>
2019-10-10 19:43:33 -0500

    Takes input from driver and sets up try and catch. Initialized an arraylist and char to be used for later methods. Using ArrayList to make life easier

--- LetterAvg.java
+++ LetterAvg.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
 
 public class LetterAvg {
+    private char input;
+    private ArrayList<String> matchStationList = new ArrayList<>();
+
+
+    public LetterAvg(){
+    }
+
+
+    public LetterAvg(char input) {
+        this.input = input;
+        PosAvg pos = new PosAvg();
+
+        try { pos.fileRead(); }
+
+        catch (Exception e) { System.out.println("error in letteravg"); }
+
+    }
+
 
 }
