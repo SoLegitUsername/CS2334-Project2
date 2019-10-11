ed5c6b1fb63e7cb3331a9fa06f4bd6a5f16f8888
Raymond <raymondandrew2000@gmail.com>
2019-10-10 19:29:46 -0500

    Initialize PosAvg and start reading Mesonet.txt to be used in other methods

--- PosAvg.java
+++ PosAvg.java
@@ -1,4 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class PosAvg {
+
+    String string;
+    private static final int stringStart = 1; //No magical numbers this time
+    private static final int stringEnd = 5; //For use in the info.subSequence
+    ArrayList<String> stationIDs = new ArrayList<>();
+
+
+    public PosAvg(){
+    }
+
+
+    public PosAvg(String str) {
+        this.string = str;
+        try{
+            fileRead();
+        }
+        catch(Exception e) {
+            System.out.println("error in PosAvg");
+        }
+    }
+
+
+    public ArrayList<String> arrayListReturn() {
+        return stationIDs;
+    }
+
+
+    public void fileRead() throws IOException {
+        String line;
+
+        FileReader filereader = new FileReader("Mesonet.txt");
+        BufferedReader br = new BufferedReader(filereader);
+        line = br.readLine();
+        line = br.readLine();
+        line = br.readLine();
+        line = br.readLine();
+
+        while (line != null) {
+            String parsedInfo = (String) line.subSequence(stringStart, stringEnd);
+            stationIDs.add(parsedInfo);
+            line = br.readLine();
+        }
+        br.close();
+    }
 
 }
